1c01449ddd66c143c478c1fa415fb01e53053863
sourabhpatel073 <sourabhpatel073@email.com>
2022-11-13 21:18:05 +0530

    a

--- scripts/details.js
+++ scripts/details.js
@@ -1,31 +1,30 @@
-import {navbar1} from "../components/navbar.js";
-import {footer} from "../components/footer.js";
+import { navbar1 } from "../components/navbar.js";
+import { footer } from "../components/footer.js";
 
-document.getElementById("navbar").innerHTML=navbar1();
-document.getElementById("footer").innerHTML=footer();
+document.getElementById("navbar").innerHTML = navbar1();
+document.getElementById("footer").innerHTML = footer();
 
-let data=JSON.parse(localStorage.getItem("details"))||[];
-const append=()=>{
-    let image=document.getElementById("image_div");
-    let img=document.createElement("img");
-    img.src=data.image;
-    image.append(img);
-    let details=document.getElementById("details_div");
-    let title=document.createElement("p");
-    title.innerText=data.name;
-    let cprice=document.createElement("h1");
-    cprice.innerText=data.price;
-    let btn=document.createElement("button");
-    btn.innerText="ADD T0 BAG";
-    btn.addEventListener("click",function(){
-        Bag()
-    })
-    let delivery=document.createElement("p");
-    delivery.innerText="Free Delivery";
+let data = JSON.parse(localStorage.getItem("details")) || [];
+const append = () => {
+  let image = document.getElementById("image_div");
+  let img = document.createElement("img");
+  img.src = data.image;
+  image.append(img);
+  let details = document.getElementById("details_div");
+  let title = document.createElement("p");
+  title.innerText = data.name;
+  let cprice = document.createElement("h1");
+  cprice.innerText = data.price;
+  let btn = document.createElement("button");
+  btn.innerText = "ADD T0 BAG";
+  btn.addEventListener("click", function () {
+    Bag();
+  });
+  let delivery = document.createElement("p");
+  delivery.innerText = "Free Delivery";
 
-    details.append(title,cprice,btn,delivery)
-    document.getElementById("image_container").append(image,details);
-
-}
+  details.append(title, cprice, btn, delivery);
+  document.getElementById("image_container").append(image, details);
+};
 
 append(data);
