dace4e89caa4d2e972f8082227dd4489db16f07b
sourabhpatel073 <sourabhpatel073@email.com>
2022-11-13 11:25:17 +0530

    bag

--- scripts/bag.js
+++ scripts/bag.js
@@ -2,6 +2,16 @@
 
 //document.getElementById("navbar").innerHTML = navbar1();
 
+import { navbar1 } from "../components/navbar.js";
+
+let navbar_div = document.getElementById("navbar");
+navbar_div.innerHTML = navbar1();
+
+import { footer } from "../components/footer.js";
+
+let footer_div = document.getElementById("footer");
+footer_div.innerHTML = footer();
+
 document.getElementById("button").onclick = () => {
   window.location.href = "checkout.html";
 };
