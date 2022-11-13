de9a17de2cdc71705c5acd56b3f2f1d25b7e9bfa
Mehul-Kanjariya <mehulktik@gmail.com>
2022-11-13 18:57:30 +0530

    Vamsra

---
+++ Admin/component/footer.js
@@ -0,0 +1,75 @@
+const footer=()=>{
+
+    return`
+    <div>
+        <div id="foot3">
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-square-snapchat"></i>
+           <p>|</p>
+            <i class="fa-brands fa-cc-visa"></i>
+            <i class="fa-brands fa-cc-mastercard"></i>
+            <i class="fa-brands fa-cc-paypal"></i>
+            <i class="fa-brands fa-cc-amex"></i>
+            <i class="fa-brands fa-cc-visa"></i>
+
+        </div>
+        <div style="background-color: #eeeeee;">
+            <div id="foot1">
+                <div>
+                    <p class="head1">HELP & INFORMATION</p>
+                    <ul>Help</ul>
+                    <ul>Track order</ul>
+                    <ul>Delivery & returns</ul>
+                    <ul>Sitemap</ul>
+                </div>
+
+                <div>
+                    <p class="head1">ABOUT ASOS</p>
+                    <ul>About us</ul>
+                    <ul>Carrers at ASOS</ul>
+                    <ul>Corporate responsibility</ul>
+                    <ul>Investor's site</ul>
+                </div>
+
+                <div>
+                    <p class="head1">MORE FROM ASOS</p>
+                    <ul>Mobile and ASOS apps</ul>
+                    <ul>ASOS Marketplace</ul>
+                    <ul>Gift vouchers</ul>
+                    <ul>Black Friday</ul>
+                    <ul>ASOS x Thrift+</ul>
+                </div>
+                <div>
+                    <p class="head1"> SHOPPING FROM :</p>
+                    <div id="hea1">
+                        <p>You're in </p>
+                        <img style=" height: 20px;" src="https://assets.asosservices.com/storesa/images/flags/in.png"
+                            alt="">
+                        <p><b>| CHANGE</b> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div style="background-color:#dddddd ;">
+        <div id="foot2">
+            <div>
+                <p>© 2022 ASOS</p>
+            </div>
+            <div style="display:flex; column-gap: 10px;">
+                <P>Privacy & Cookies |</P>
+                <p>Ts&Cs |</p>
+                <p>Accessibility</p>
+
+            </div>
+
+        </div>
+    </div>
+    </div> 
+    `
+
+}
+
+export { footer } ;
---
+++ Admin/index.js
@@ -0,0 +1,84 @@
+import {navbar1} from './component/navbar.js';
+
+let navbar_div = document.getElementById('navbar');
+navbar_div.innerHTML=navbar1();
+
+import {footer} from './component/footer.js';
+
+let footer_div = document.getElementById('footer');
+footer_div.innerHTML=footer();
+
+const getdata = async () => {
+    try{
+        let data = await fetch('https://agile-badlands-40822.herokuapp.com/posts')
+        let actual_data = await data.json();
+        appenddata(actual_data);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+// const append = (data) => {
+//     let div = document.getElementById('posts');
+//     data.forEach((el) => {
+//         console.log(el)            
+//             let post_div = document.createElement("div");
+//             let img =  document.createElement("img");
+//             img.src = el.image_url;
+
+//             let caption = document.createElement("p");
+//             caption.textContent = el.name;
+
+//             let price = document.createElement("p");
+//             price.textContent = el.price;
+
+//             post_div.append(img,caption,price);
+//             div.append(post_div);
+//     })
+// }
+
+const appenddata=(data)=>{
+    let load=document.getElementById("loader");
+    load.style.display="none"
+    document.getElementById("container").innerHTML=null
+    data.forEach((el)=>{
+        let div=document.createElement("div");
+
+        let img=document.createElement("img");
+        img.src=el.image;
+
+        let h3=document.createElement("p");
+        h3.innerText=el.name;
+
+        let h2=document.createElement("h3");
+        h2.innerText=el.price;
+
+        let product_id=document.createElement("p");
+        product_id.innerText=el.id;
+
+        div.append(img,h3,h2,product_id);
+        document.getElementById("container").append(div);
+    })
+}
+
+// debounce
+
+    
+// loader
+
+let id;
+debounce(getdata,1000);
+
+function debounce(func,delay){
+if(id)
+{
+    clearTimeout(id)
+    // loader=document.getElementById("loader");
+    //     loader.style.display="block"
+}
+id=setTimeout(function(){
+    func()
+    // loader=document.getElementById("loader");
+    //     loader.style.display="none"
+},delay)
+}
